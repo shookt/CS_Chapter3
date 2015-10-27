4d8e82e494904f0395e7c01014792422ac8098d3
shookt <tshook428@gmail.com>
2015-10-27 10:39:59 -0400

    first commit

--- CS_Chapter3/CheckLowRate/Program.cs
+++ CS_Chapter3/CheckLowRate/Program.cs
@@ -9,6 +9,16 @@ namespace CheckLowRate
     {
         static void Main(string[] args)
         {
+            string rate;
+            double pay, actual;
+            Console.WriteLine("Enter pay rate");
+            rate = Console.ReadLine();
+            pay = Convert.ToDouble(rate);
+            actual = 5.65;
+            if (pay < actual)
+            {
+                Console.WriteLine("Go fuck yourself.");
+            }
         }
     }
 }
