3bb4923f724c71fd33a5a555d61ab3792fa9c83c
shookt <tshook428@gmail.com>
2015-10-27 11:33:20 -0400

    sddddfghjkl

--- CS_Chapter3/EnsureValidPayRate/Program.cs
+++ CS_Chapter3/EnsureValidPayRate/Program.cs
@@ -9,6 +9,25 @@ namespace EnsureValidPayRate
     {
         static void Main(string[] args)
         {
+            string rate, rate2;
+            double pay, pay2, actual, actual2, weeklypay;
+            Console.WriteLine("Enter pay rate");
+            rate = Console.ReadLine();
+            pay = Convert.ToDouble(rate);
+            actual = 5.65;
+            actual2 = 49.99;
+            weeklypay = pay * 40;
+            if (pay < actual || pay > actual2)
+            {
+                Console.WriteLine("Enter pay rate again");
+                rate2 = Console.ReadLine();
+                pay2 = Convert.ToDouble(rate2);
+                if (pay2 < actual || pay2 > actual2)
+                    Console.WriteLine("Error");
+            }
+            else
+                Console.WriteLine(weeklypay);
+                
         }
     }
 }
