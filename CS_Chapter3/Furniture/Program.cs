c1ca338d3872570aae329828a9155a5e57311924
shookt <tshook428@gmail.com>
2015-10-29 11:21:50 -0400

    fifth commit

--- CS_Chapter3/Furniture/Program.cs
+++ CS_Chapter3/Furniture/Program.cs
@@ -9,7 +9,27 @@ namespace Furniture
     {
         static void Main(string[] args)
         {
-
+            string input, pricep, priceo, pricem;
+            double p, o, m;
+            p = 100;
+            o = 225;
+            m = 310;
+            pricep = p.ToString("C");
+            priceo = o.ToString("C");
+            pricem = m.ToString("C");
+            Console.WriteLine("Price of pine is {0}.", pricep);
+            Console.WriteLine("Price of oak is {0}.", priceo);
+            Console.WriteLine("Price of mahogany is {0}.", pricem);
+            Console.WriteLine("What wood do you want? 'P' for pine, 'O' for oak, 'M' for mahogany.");
+            input = Console.ReadLine();
+            if (input == "P")
+                Console.WriteLine(pricep);
+            if (input == "O")
+                Console.WriteLine(priceo);
+            if (input == "M")
+                Console.WriteLine(pricem);
+            else
+                Console.WriteLine("the price is $0");
         }
     }
 }
