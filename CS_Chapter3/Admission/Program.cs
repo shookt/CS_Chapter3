732106b4147ed432ddd94cd21f3c1cfa1abb873f
shookt <tshook428@gmail.com>
2015-11-04 11:33:20 -0500

    sixth commit

--- CS_Chapter3/Admission/Program.cs
+++ CS_Chapter3/Admission/Program.cs
@@ -9,6 +9,20 @@ namespace Admission
     {
         static void Main(string[] args)
         {
+            string input, input2;
+            double num, num2;
+            Console.WriteLine("Enter your GPA.");
+            input = Console.ReadLine();
+            Console.WriteLine("Enter your test score.");
+            input2 = Console.ReadLine();
+            num = Convert.ToDouble(input);
+            num2 = Convert.ToDouble(input2);
+            if (num >= 3.0 && num2 >= 60)
+                Console.WriteLine("Accept.");
+            else if (num < 3.0 && num2 >= 80)
+                Console.WriteLine("Accept.");
+            else
+                Console.WriteLine("Reject.");
         }
     }
 }
