1401c73c21ccc1c432383781848f7ac89eb9c92c
shookt <tshook428@gmail.com>
2015-11-06 10:48:54 -0500

    last commit

--- CS_Chapter3/Payroll/Program.cs
+++ CS_Chapter3/Payroll/Program.cs
@@ -9,6 +9,24 @@ namespace Payroll
     {
         static void Main(string[] args)
         {
+            string input, input2;
+            double pay, hours, gross, withhold, net;
+            Console.WriteLine("Input your hourly pay rate.");
+            input = Console.ReadLine();
+            pay = Convert.ToDouble(input);
+            Console.WriteLine("Input the amount of hours you work.");
+            input2 = Console.ReadLine();
+            hours = Convert.ToDouble(input2);
+            gross = pay * hours;
+            withhold = 0;
+            if (gross <= 300)
+            withhold = withhold + .10;
+            else if (gross > 300)
+                withhold = withhold + .12;
+            net = gross - (withhold * gross);
+            Console.WriteLine(gross);
+            Console.WriteLine(withhold);
+            Console.WriteLine(net);
         }
     }
 }
